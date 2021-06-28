3a0c55c8721e942540de9f8bdae4285395f6f7d6
Bikal Lem <gbikal@gmail.com>
2021-06-28 13:27:28 +0100

    add copyright, .mli

--- lib/summer.ml
+++ lib/summer.ml
@@ -1,4 +1,18 @@
+(*-------------------------------------------------------------------------
+ * Copyright (c) 2019, 2020 Bikal Gurung. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License,  v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * %%NAME%% %%VERSION%%
+ *-------------------------------------------------------------------------*)
+
 open Lwt.Infix
+
+(* Summer is a http 1.1 server as outlined in the following RFCs
+   1. https://datatracker.ietf.org/doc/html/rfc7230
+   2. https://datatracker.ietf.org/doc/html/rfc7231 *)
 
 (* let handle_connection _client_addr _client_socket = Lwt.return_unit *)
 
--- lib/summer.mli
+++ lib/summer.mli
@@ -8,9 +8,5 @@
  * %%NAME%% %%VERSION%%
  *-------------------------------------------------------------------------*)
 
-let () =
-  let port = ref 3000 in
-  Arg.parse
-    [("-p", Arg.Set_int port, " Listening port number (3000 by default)")]
-    ignore "A echo HTTP server using summer" ;
-  Summer.start !port (fun _addr _fd -> Lwt.return ())
+val start :
+  int -> (Lwt_unix.sockaddr -> Lwt_unix.file_descr -> unit Lwt.t) -> 'a
