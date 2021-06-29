6816607bb1975431049a5537658c32ca1a485b9f
Bikal Lem <gbikal@gmail.com>
2021-06-29 22:48:21 +0100

    fmt

--- lib/summer.ml
+++ lib/summer.ml
@@ -217,12 +217,12 @@ let handle_connection request_handler client_addr fd =
         |> Lwt_unix.write_string fd response_txt 0
         >|= fun _ -> ())
 
-let start ~port handler =
+let start ~port request_handler =
   let listen_address = Unix.(ADDR_INET (inet_addr_loopback, port)) in
   Lwt_engine.set (new Lwt_engine.libev ()) ;
   Lwt.async (fun () ->
       Lwt_io.establish_server_with_client_socket ~backlog:11_000 listen_address
-        (handle_connection handler)
+        (handle_connection request_handler)
       >>= fun _server -> Lwt.return () ) ;
   let forever, _ = Lwt.wait () in
   Lwt_main.run forever
