e3d0d7019477c9014d7a2da25657e80ea52dce79
Bikal Lem <gbikal@gmail.com>
2021-06-30 11:39:47 +0100

    update text in examples/echo

--- examples/echo.ml
+++ examples/echo.ml
@@ -12,7 +12,7 @@ let () =
   let port = ref 3000 in
   Arg.parse
     [("-p", Arg.Set_int port, " Listening port number (3000 by default)")]
-    ignore "A echo HTTP server using summer" ;
+    ignore "An echo HTTP server using summer!" ;
   Summer.start ~port:!port (fun ctx ->
       Summer.(
         Context.request ctx
