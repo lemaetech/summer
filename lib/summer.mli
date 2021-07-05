855a99e9fa2d2a9ac04288e420203a35278fbe53
Bikal Lem <gbikal@gmail.com>
2021-07-05 16:47:20 +0100

    fmt

--- lib/summer.mli
+++ lib/summer.mli
@@ -47,10 +47,10 @@ module Request : sig
       (** Represented by '*': The asterisk "*" symbol in an Accept-Encoding
           field matches any available content-coding not explicitly listed in
           the header field. *)
-    | `None  (** Represented by '' empty value.*)
+    | `None  (** Represented by empty - ''- encoding value. *)
     | `Other of string
       (** Any other encoding - possibly a custom one - not specified by the HTTP
-          RFC 7230 or 7231 or 7932.*) ]
+          RFC 7230 or 7231 or 7932. *) ]
 
   val meth : t -> meth
   val request_target : t -> string
