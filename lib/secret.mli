f457077dd6b9e40f1581e9e5ac28f78077d2ed09
Bikal Lem <gbikal@gmail.com>
2021-08-11 23:40:13 +0100

    fmt

--- lib/secret.mli
+++ lib/secret.mli
@@ -2,20 +2,15 @@ module Key : sig
   type t
 
   val size : int
-  (** [size] returns the size of [t] in bytes. *)
+  (** [size] returns the size of {!type:t} in bytes. *)
 
   val raw : t -> Cstruct.t
   (** [raw t] returns raw key bytes. *)
 
   val create : unit -> t
-
-  val of_base64 : string -> (t, string) result
-  (** [parse_key s] parses and decodes [s] from base64 string into a [t]. *)
-
-  val to_base64 : t -> string
-  (** [base64 t] encodes [t] into a base64 format string. *)
-
   val of_string : string -> (t, string) result
+  val of_base64 : string -> (t, string) result
+  val to_base64 : t -> string
 end
 
 (** Represents an encrypted secret content. *)
